b35f56955c8aa658e7a4b5dd54f7519fe4ffa6f8
framewichaiwong <33681288+framewichaiwong@users.noreply.github.com>
2021-07-14 16:56:07 +0700

    edit encode

--- src/main/java/com/example/menu/controller/UserManagerController.java
+++ src/main/java/com/example/menu/controller/UserManagerController.java
@@ -65,6 +65,7 @@ public class UserManagerController {
         Optional<UserManager> checkId = userManagerRepository.findById(managerId);
         UserManager checkUserManager = userManagerRepository.findByUserName(userManager.getUserName());
         if(checkId.isPresent() && checkUserManager != null) {
+            userManager.setPassWord(encoderUtil.passwordEncoder().encode(userManager.getPassWord()));
             UserManager user = userManagerService.updateUserManager(userManager);
             response.setStatus(1);
             response.setMessage("Update Success");
