972312e682a8558235519d27342893c0e60a8c26
framewichaiwong <33681288+framewichaiwong@users.noreply.github.com>
2022-03-24 15:31:24 +0700

    แก้ไข jdk 17 เป็น jdk 18 และ version ของ swagger

--- src/main/java/com/example/menu/config/Swagger.java
+++ src/main/java/com/example/menu/config/Swagger.java
@@ -4,9 +4,13 @@ package com.example.menu.config;
 import com.example.menu.oauth2.AuthenticationResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
@@ -15,39 +19,56 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.*;
 
+@EnableWebMvc
 @Configuration
 @EnableSwagger2
 public class Swagger implements WebMvcConfigurer {
 
-    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json"));
+//    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json"));
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//
+//    @Bean
+//    public Docket api() {
+//        ParameterBuilder parameterBuilder = new ParameterBuilder();
+//        parameterBuilder.name("Authorization")
+//                .modelRef(new ModelRef("string"))
+//                .parameterType("header")
+//                .description("JWT token")
+//                .required(true)
+//                .build();
+//        List<Parameter> parameters = new ArrayList<>();
+//        parameters.add(parameterBuilder.build());
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .produces(DEFAULT_PRODUCES_CONSUMES)
+//                .consumes(DEFAULT_PRODUCES_CONSUMES)
+//                .select()
+//                .build()
+//                .ignoredParameterTypes(AuthenticationResource.class)
+//                .globalOperationParameters(parameters);
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
-
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Bean
     public Docket api() {
-        ParameterBuilder parameterBuilder = new ParameterBuilder();
-        parameterBuilder.name("Authorization")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .description("JWT token")
-                .required(true)
-                .build();
-        List<Parameter> parameters = new ArrayList<>();
-        parameters.add(parameterBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
-                .produces(DEFAULT_PRODUCES_CONSUMES)
-                .consumes(DEFAULT_PRODUCES_CONSUMES)
                 .select()
-                .build()
-                .ignoredParameterTypes(AuthenticationResource.class)
-                .globalOperationParameters(parameters);
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
-
 }
